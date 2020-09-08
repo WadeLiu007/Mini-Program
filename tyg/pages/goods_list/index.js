f342be9fdb25362be9876c418c41ce3bcba969e2
WadeLiu007 <846890195@qq.com>
2020-09-08 23:13:32 +0800

    详情页刚开始

--- tyg/pages/goods_list/index.js
+++ tyg/pages/goods_list/index.js
@@ -1,9 +1,38 @@
 //Page Object
 Page({
   data: {
-    
+    tabs:[{
+      id:0,
+      value:'综合',
+      isActive:true
+    },{
+      id:1,
+      value:'销量',
+      isActive:false
+    },{
+      id:2,
+      value:'评价',
+      isActive:false
+    }],
+    currentIndex:0
   },
-  //options(Object)
+
+  handleTabsItemChange(e){
+    let {index} = e.detail;
+    // 修改原数组;
+    let {tabs} = this.data;
+    tabs.forEach((item,i)=>{
+      if(i === index){
+        item.isActive = true;
+      }else{
+        item.isActive = false
+      }
+    })
+
+    this.setData({
+      tabs
+    })
+  },
   onLoad: function(options){
     console.log(options);
   },
