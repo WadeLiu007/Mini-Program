73b33566d867da7595df01f02670ff288f7e778e
WadeLiu007 <846890195@qq.com>
2020-09-11 13:27:20 +0800

    支付页面初稿

--- tyg/pages/pay/index.js
+++ tyg/pages/pay/index.js
@@ -1,66 +1,43 @@
-// pages/pay/index.js
+/*1. 从缓存中获取购物车选择为true的值 */
+import {
+  getSetting,
+  chooseAddress,
+  openSetting
+} from '../../utils/asyncWx.js';
+import regeneratorRuntime from '../../lib/runtime/runtime';
+
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    address: {},
+    cart: [],
+    totalPrice: 0,
+    totalNum: 0
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
+    // 1.获取缓存中的地址;
+    let address = wx.getStorageSync('address');
+    let cart = wx.getStorageSync('cart') || [];
+    
+    // 过滤购物车中选择为true的;
+    cart = cart.filter(item => item.checked);
 
-  },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    // 计算总价格和总数量：
+    let totalPrice = 0;
+    let totalNum = 0;
+    cart.forEach(item => {
+        totalNum += item.num;
+        totalPrice += item.num * item.goods_price;
+    })
 
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    this.setData({
+      cart,
+      address,
+      totalPrice,
+      totalNum
+    });
   }
+
 })
